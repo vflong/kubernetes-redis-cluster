391f017e71318cc613cc2c422b75f59602918d68
feilong <weifeilong2013@gmail.com>
2018-06-27 20:28:16 +0800

    add pod/svc name prefix lifesense-qa

--- statefulset-cleanup.sh
+++ statefulset-cleanup.sh
@@ -1,5 +1,5 @@
-kubectl delete -f persistentvolume -n base
-kubectl delete -f persistentvolumeclaims -n base
-kubectl delete cm redis-conf -n base
-kubectl delete -f statefulset-services -n base
 kubectl delete -f statefulset -n base
+kubectl delete -f statefulset-services -n base
+kubectl delete cm redis-conf -n base
+kubectl delete -f persistentvolumeclaims -n base
+kubectl delete -f persistentvolume
